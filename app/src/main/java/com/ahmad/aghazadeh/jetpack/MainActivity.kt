48327045d15d450119fa32d19c5989c14464efb7
ahmad.aghazadeh <Ahmad.aghazadeh.a@gmail.com>
2023-07-17 23:15:36 +0330

    Add profile card

--- app/src/main/java/com/ahmad/aghazadeh/jetpack/MainActivity.kt
+++ app/src/main/java/com/ahmad/aghazadeh/jetpack/MainActivity.kt
@@ -3,19 +3,26 @@ package com.ahmad.aghazadeh.jetpack
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CardElevation
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.ahmad.aghazadeh.jetpack.ui.theme.JetpackTheme
@@ -39,12 +46,29 @@ fun CreateBizCard(){
     Surface(modifier = Modifier
         .fillMaxWidth()
         .fillMaxHeight()) {
-        Card(modifier = Modifier.width(200.dp)
+        Card(modifier = Modifier
+            .width(200.dp)
             .height(390.dp)
-            .padding(8.dp),
+            .padding(12.dp),
             shape = RoundedCornerShape(corner = CornerSize(15.dp)),
-            elevation = CardElevation(4.dp)
+            elevation = CardDefaults.cardElevation(
+                defaultElevation = 10.dp
+            ),
+            colors = CardDefaults.cardColors(
+                containerColor = MaterialTheme.colorScheme.surfaceVariant
+            )
         ) {
+            Surface(modifier = Modifier
+                .size(150.dp)
+                .padding(5.dp),
+            shape = CircleShape,
+            border = BorderStroke(0.5.dp, Color.LightGray),
+                shadowElevation = 4.dp,
+                color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.5f)
+            ) {
+                Image(painter = painterResource(id = R.drawable.ic_launcher_foreground), contentDescription ="Profile image",
+                modifier = Modifier.width(150.dp))
+            }
         }
     }
 }
