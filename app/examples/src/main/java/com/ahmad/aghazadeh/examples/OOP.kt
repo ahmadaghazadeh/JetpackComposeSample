c256ae3953321594586899a026cef0c47bed81ab
ahmad.aghazadeh <Ahmad.aghazadeh.a@gmail.com>
2023-07-22 19:15:38 +0330

    Kotlin OOP 4

--- app/examples/src/main/java/com/ahmad/aghazadeh/examples/OOP.kt
+++ app/examples/src/main/java/com/ahmad/aghazadeh/examples/OOP.kt
@@ -20,29 +20,36 @@ fun main() {
 //
 //    println(" Use Extention Method".removeFirstAndLastChar())
 //
-    val ahmad = Person(name = "Ahmad", lastName = "Aghazadeh", age = 41)
-    val hamed = Person(name = "Hamed", lastName = "Roshangar", age = 41)
-    val shahrooz = Person(name = "Shahrooz", lastName = "Ansari", age = 41)
+//    val ahmad = Person(name = "Ahmad", lastName = "Aghazadeh", age = 41)
+//    val hamed = Person(name = "Hamed", lastName = "Roshangar", age = 41)
+//    val shahrooz = Person(name = "Shahrooz", lastName = "Ansari", age = 41)
+//
+//    val listOfPerson = listOf(ahmad, hamed, shahrooz)
+//    val finderPerson = Finder<Person>(listOfPerson)
+//    finderPerson.findItem(hamed) {
+//        println(it)
+//    }
+//
+//    val listOfString = listOf<String>("Ahmad", "Aghazadeh", "Ahmad", "A")
+//    val finder = Finder(listOfString)
+//
+//    finder.findItem("Ahmad") {
+//        println(it)
+//    }
+//
+//    val listofInt = listOf<Int>(1, 2, 3, 4, 5, 6, 7, 8, 9)
+//    val finderint = Finder<Int>(listofInt)
+//    finderint.findItem(3) {
+//        println(it)
+//    }
 
-    val listOfPerson = listOf(ahmad, hamed, shahrooz)
-    val finderPerson = Finder<Person>(listOfPerson)
-    finderPerson.findItem(hamed) {
-        println(it)
-    }
+    var input = Result.ERROR
+    getResult(input)
 
-    val listOfString = listOf<String>("Ahmad", "Aghazadeh", "Ahmad", "A")
-    val finder = Finder(listOfString)
-
-    finder.findItem("Ahmad") {
-        println(it)
-    }
-
-    val listofInt = listOf<Int>(1, 2, 3, 4, 5, 6, 7, 8, 9)
-    val finderint = Finder<Int>(listofInt)
-    finderint.findItem(3) {
-        println(it)
-    }
-
+    Repository.startFetch()
+    getResult(result = Repository.getCurrentLoadState())
+    Repository.finishFetch()
+    getResult(result = Repository.getCurrentLoadState())
 
 }
 
@@ -103,8 +110,43 @@ class Finder<T>(private val list: List<T>) {
     }
 }
 
+fun getResult(result: Result) {
+    return when (result) {
+        Result.SUCCESS -> println("SUCCESS")
+        Result.FAILURE -> println("FAILURE")
+        Result.ERROR -> println("ERROR")
+        Result.IDLE -> println("IDLE")
+        Result.LOADING -> println("LOADING")
+    }
+}
+
 enum class Result {
     SUCCESS,
     FAILURE,
-    ERROR
+    ERROR,
+    IDLE,
+    LOADING
+}
+
+object Repository {
+    private var LoadState: Result = Result.IDLE
+    private var dataFetched: String? = null
+    fun startFetch() {
+        LoadState = Result.LOADING
+        dataFetched = "data"
+    }
+
+    fun finishFetch() {
+        LoadState = Result.SUCCESS
+        dataFetched = null
+    }
+
+    fun errorFetch() {
+        LoadState = Result.ERROR
+        dataFetched = null
+    }
+
+    fun getCurrentLoadState(): Result {
+        return LoadState
+    }
 }
