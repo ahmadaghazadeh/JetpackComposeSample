85760ab11bea22eebf53889b8593154b753b32b1
ahmad.aghazadeh <Ahmad.aghazadeh.a@gmail.com>
2023-07-22 18:36:02 +0330

    Kotlin OOn 2P

--- app/examples/src/main/java/com/ahmad/aghazadeh/examples/OOP.kt
+++ app/examples/src/main/java/com/ahmad/aghazadeh/examples/OOP.kt
@@ -8,7 +8,23 @@ fun main() {
     val truck = Truck("Blue", "Volvo")
     truck.drive()
     truck.speedUp()
+
+    val button = Button("First")
+    button.onClick("New Message")
+
+    val name: String = "Ahmad"
+
+    val newString = name.append(" Use Extention Method")
+
+    println(newString)
+
+    println(" Use Extention Method".removeFirstAndLastChar())
+
+    val person = Person(name = "Ahmad", lastName = "Aghazadeh", age = 41)
+    println(person)
 }
+
+data class Person(val name: String, val lastName: String, val age: Int)
 
 open class Car(private var color: String, private var model: String) {
 
@@ -32,3 +48,23 @@ class Truck(color: String, model: String) : Car(color, model) {
         println("xx")
     }
 }
+
+class Button(val label: String) : ClickEvent {
+    override fun onClick(message: String) {
+        println("Clicked by $message")
+    }
+
+}
+
+interface ClickEvent {
+    fun onClick(message: String)
+}
+
+fun String.append(toAppend: String): String {
+    return this + toAppend
+}
+
+fun String.removeFirstAndLastChar(): String {
+    return substring(1, length - 1)
+}
+
